4341f99491eac4dce2017d2d03cbdd69b61680f9
Rafael <rafael.maciel@showtecnologia.com>
2022-07-19 11:28:48 -0300

    cridando o menu dropdown

---
+++ src/components/MenuPetItens.js
@@ -0,0 +1,17 @@
+export const MenuPetItens = [
+  {
+    title: 'Cachorro',
+    path: '/cachorro',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Gatos',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Outros',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+];
