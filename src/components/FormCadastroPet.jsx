21ee5d61b58cae8e20f4f9fbf8ac3df5fe66e5f3
Rafael <rafael.maciel@showtecnologia.com>
2022-08-11 17:36:10 -0300

    tentando implementar multiplas imgs

--- src/components/FormCadastroPet.jsx
+++ src/components/FormCadastroPet.jsx
@@ -1,67 +1,68 @@
 import { useState, useEffect, useReducer, useContext } from "react";
-import { db,storage } from "../config/firebase";
+import { db, storage } from "../config/firebase";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
 import { async } from "@firebase/util";
-import { ref, uploadBytesResumable,getDownloadURL } from "firebase/storage"; 
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export default function FormCadastroPet() {
-    const [formaData, setFormaData] = useState({
-        nome: "",
-        tipo: "",
-        porte: "",
-        telefone: "",
-        estado: "",
-        cidade: "",
-        descricao: "",
-        foto: "",
-    });
-
+    const [imgUrlPrincipal, setImgUrlPrincipal] = useState("");
     const [imgUrl, setImgUrl] = useState("");
-    const [ progress , setProgress] = useState(0);
+    const [imgsObj, setImgsObj] = useState([]);
+    const [progress, setProgress] = useState(0);
+
+    function getUrlsForm (e) {
+        
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            let storageRef = ref(storage, `images/${e.target.fotosPet.files[0].name }`); 
-            let uploadTask = uploadBytesResumable(storageRef, e.target.fotosPet.files[0]);
-            uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                    console.log(snapshot);
-                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    setProgress(progress);
-                },
-                (error) => {
-                    console.log(error);
-                },
-                async () => {
-                    await getDownloadURL(uploadTask.snapshot.ref ).then((downloadURL) => {
-                        setImgUrl(downloadURL);
-                    });
-                }
-            );
-
+             for (let i = 0; i < e.target.fotosPet.files.length; i++) {
+                const file = e.target.fotosPet.files[i];
+                const filePath = `${file.name}`;
+                file["id"] = filePath;
+                setImgUrl((imgUrl) => [...imgUrl, file]);
+            }
+            const promises = []
+                imgUrl.map((image) => {
+                    let storageRef = ref(storage, `images/${image.name}`);
+                    let uploadTask = uploadBytesResumable(storageRef, image);
+                    promises.push(uploadTask);
+                    uploadTask.on(
+                        "state_changed",
+                        (snapshot) => {
+                            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            setProgress(progress);
+                        },
+                        (error) => {
+                            console.log(error);
+                        },
+                        async () => {
+                            await getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                console.log(downloadURL);
+                                setImgsObj((imgsObj) => [...imgsObj, downloadURL]);
+                            });
+                        }
+                    );
+                });
+            Promise.all(promises)
+            console.log(imgsObj);
             await addDoc(collection(db, "pets"), {
                 nomePet: e.target.nomePet.value,
                 tipoPet: e.target.tipoPet.value,
-                    portePet: e.target.portePet.value,
-                    telefone: e.target.telefonePet.value,
-                    estadoPet: e.target.estadoPet.value,
-                    cidadePet: e.target.cidadePet.value,
-                    descricaoPet: e.target.descricaoPet.value,
-                    imgPet: imgUrl,
-                })
-                .then(() => {
-                    console.log("Pet cadastrado com sucesso!");
-                    
-                });
-
-            } catch (error) {
+                portePet: e.target.portePet.value,
+                telefone: e.target.telefonePet.value,
+                estadoPet: e.target.estadoPet.value,
+                cidadePet: e.target.cidadePet.value,
+                descricaoPet: e.target.descricaoPet.value,
+                imgPet: imgsObj[0],
+            }).then(() => {
+                console.log("Pet cadastrado com sucesso!");
+            });
+        } catch (error) {
             console.log(error);
         }
     };
-
-
 
     return (
         <form onSubmit={handleSubmit}>
@@ -171,11 +172,16 @@ export default function FormCadastroPet() {
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                         Fazer upload de fotos{" "}
                     </label>
-                    <input type="file" id="fotosPet" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input
+                        type="file"
+                        multiple
+                        id="fotosPet"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
                 </div>
             </div>
             <div className="flex justify-end">
-            {!imgUrl && <progress className="progress" value={progress} max="100" />}
+                {!imgUrlPrincipal && <progress className="progress" value={progress} max="100" />}
             </div>
             <button
                 type="submit"
